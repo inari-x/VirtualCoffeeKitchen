073f4459bcca84b764be0fab8cbccabc86dae839
Lara Pourabdolrahim <Lara.Pourabdolrahim.ext@bestsecret.com>
2023-02-13 12:06:35 +1100

    API call to create meeting is working and returns join link

--- coffee-kitchen-server/README.md
+++ coffee-kitchen-server/README.md
@@ -4,7 +4,7 @@ https://docs.docker.com/language/python/run-containers/
 
 From your built image, run ```$ docker run -d -p 8000:5000 python-docker```. Your app will now run on localhost:8000 in your browser. 
 
-To run locally, run ```$ python3 -m venv .venv
+To run locally (without using the Docker), run ```$ python3 -m venv .venv
 $ source .venv/bin/activate``` to activate the environment you are working in. 
 Next, run ```$ pip3 install -r requirements.txt``` to install Flask and its dependencies.
 Then, run ```$ python3 kitchen.py``` and open "localhost:5000" in your browser. 
--- coffee-kitchen-server/kitchen.py
+++ coffee-kitchen-server/kitchen.py
@@ -1,10 +1,33 @@
 from flask import Flask
+import requests
+import datetime
+import os
+import jwt
 
 app = Flask(__name__)
+
+def get_zoom_token():
+    return os.environ['ZOOM_TOKEN']
 
 @app.route('/')
 def hello_world():
   return 'Hello, Docker!'
+
+
+@app.route('/participant-count')
+def participant_count():
+  return '42'
+
+@app.route('/create-meeting')
+def zoom_api():
+  j = {
+   "settings": {
+    "join_before_host": True
+    }}
+  r = requests.post(f"https://api.zoom.us/v2/users/larapour@googlemail.com/meetings", headers = {
+    'authorization': f"bearer {get_zoom_token()}", 'content-type': 'application/json'}, json = j)
+  return r.json().get('join_url')
+
 
 if __name__ == '__main__':
   app.run(host='::', debug=True)
